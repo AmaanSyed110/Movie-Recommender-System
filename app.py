9d52d8d9f5eef4bb1c47561113e38d3a9b423969
AmaanSyed110 <amaansyed2001.as@gmail.com>
2024-06-03 13:37:43 +0530

    Update App

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ def recommend(movie):
 
 
 st.header('Movie Recommender System Using Machine Learning')
-movies = pickle.load(open('artifacts/movie_list.pkl','rb'))
-similarity = pickle.load(open('artifacts/similarity.pkl','rb'))
+movies = pickle.load('movie_list.pkl','rb')
+similarity = pickle.load('similarity.pkl','rb')
 
 movie_list = movies['title'].values
 selected_movie = st.selectbox(
